60712b1306cb4dcf63e1dcaf625483d821e6da0e
Alexander <lowry.alex@live.com>
2019-12-27 00:05:47 -0600

    Added frame-switching functionality.

--- ProgramWindow.py
+++ ProgramWindow.py
@@ -7,11 +7,14 @@ class Window(Frame):
         Frame.__init__(self, master)
         self.master = master
         self.controller = controller
+        self.frames = {}
 
-        self.pack(fill=BOTH, expand=1)
+        self.pack(side="top", fill="both", expand=True)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        exit_button = Button(self, text="Exit", command=self.v_exit)
-        exit_button.place(x=0, y=0)
+        # exit_button = Button(self, text="Exit", command=self.v_exit)
+        # exit_button.place(x=0, y=0)
 
         self.add_menu_bar()
         # self.add_file_drop()
@@ -20,31 +23,22 @@ class Window(Frame):
 
         self.add_context_drop()
 
+        self.add_menu_frame()
+        self.add_persons_frame()
+        self.add_institutions_frame()
+
+        self.show_frame('Menu')
+
     def add_menu_bar(self):
         self.menu = Menu(self.master)
         self.master.config(menu=self.menu)
 
     def add_context_drop(self):
         context_menu = Menu(self.menu)
-        context_menu.add_command(label="Persons", command=self.persons_context())
-        context_menu.add_command(label="Institutions", command=self.institutions_context())
+        context_menu.add_command(label="Main Menu", command=lambda: self.show_frame('Menu'))
+        context_menu.add_command(label="Persons", command=lambda: self.show_frame('Persons'))
+        context_menu.add_command(label="Institutions", command=lambda: self.show_frame('Institutions'))
         self.menu.add_cascade(label="Context", menu=context_menu)
-
-    def add_file_drop(self):
-        file_menu = Menu(self.menu)
-        file_menu.add_command(label="New")
-        file_menu.add_command(label="Open")
-        file_menu.add_command(label="Save")
-        file_menu.add_command(label="Save as...")
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.v_exit);
-        self.menu.add_cascade(label="File", menu=file_menu)
-
-    def add_edit_drop(self):
-        edit_menu = Menu(self.menu)
-        edit_menu.add_command(label="Duplicate")
-        edit_menu.add_command(label="Time")
-        self.menu.add_cascade(label="Edit", menu=edit_menu)
 
     def add_calc_drop(self):
         calc_menu = Menu(self.menu)
@@ -59,11 +53,43 @@ class Window(Frame):
         # User exit (0).
         # SystemC.exit(0)
 
-    def persons_context(self):
-        print('Switch to persons context')
+    def add_menu_frame(self):
+        main_menu_frame = Frame(self, bg="#DEDEDE")
+        main_menu_frame.pack(expand=True, fill='both')
+        self.frames.update({'Menu': main_menu_frame})
 
-    def institutions_context(self):
-        print('Switch to institutions context')
+    def show_menu_frame(self):
+        print('Switching to main menu.')
+        self.show_frame('Menu')
+
+    def add_persons_frame(self):
+        persons_frame = Frame(self, bg="#CCCCCC")
+        persons_frame.pack(fill=BOTH, expand=True)
+        button = Button(persons_frame, text="Persons")
+        button.pack()
+        self.frames.update({'Persons': persons_frame})
+
+    def show_persons_frame(self):
+        print('Switching to persons.')
+        self.show_frame('Persons')
+
+    def add_institutions_frame(self):
+        institutions_frame = Frame(self, bg="#EEAAAA")
+        institutions_frame.pack(fill=BOTH, expand=True)
+        button = Button(institutions_frame, text="Institutions")
+        button.pack()
+        self.frames.update({'Institutions': institutions_frame})
+
+    def show_institutions_frame(self):
+        print('Switching to institutions.')
+        self.show_frame('Institutions')
+
+    def show_frame(self, name):
+        for key, value in self.frames.items():
+            value.pack_forget()
+        frame = self.frames[name]
+        frame.pack(fill=BOTH, expand=True)
+        # frame.tkraise()
 
 
 def show_window(controller):
@@ -71,7 +97,14 @@ def show_window(controller):
     app = Window(root, controller)
 
     root.wm_title("OffBook Lite")
-    root.geometry("320x200");
+    # root.geometry("320x200");
+
+    # root.attributes('-fullscreen', True)  #fullscreens the window
+
+    # w, h = root.winfo_screenwidth(), root.winfo_screenheight()  #resizes window to screen size
+    # root.geometry("%dx%d+0+0" % (w, h))
+
+    root.state('zoomed')    # maximizes window
 
     root.mainloop()  # show window
 
