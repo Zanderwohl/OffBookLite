fdff35a2687d80031e10df5b0fdc72db40f84960
Alexander <lowry.alex@live.com>
2019-12-27 20:57:47 -0600

    Removed superfluous code.

--- ProgramWindow.py
+++ ProgramWindow.py
@@ -9,18 +9,13 @@ class Window(Frame):
         self.controller = controller
         self.frames = {}
         self.current_frame = None
+        self.Menu = None
 
         self.pack(side="top", fill="both", expand=True)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        # exit_button = Button(self, text="Exit", command=self.v_exit)
-        # exit_button.place(x=0, y=0)
-
         self.add_menu_bar()
-        # self.add_file_drop()
-        # self.add_edit_drop()
-        # self.add_calc_drop()
 
         self.add_context_drop()
 
@@ -51,53 +46,34 @@ class Window(Frame):
 
     def v_exit(self):
         self.controller.exit(1)
-        # User exit (0).
-        # SystemC.exit(0)
 
     def add_menu_frame(self):
         main_menu_frame = Frame(self, bg="#DEDEDE")
-        # main_menu_frame.pack(expand=True, fill='both')
         button_persons = Button(main_menu_frame, text="Persons", command=lambda: self.show_frame('Persons'))
         button_persons.pack()
         button_institutions = Button(main_menu_frame, text="Institutions", command=lambda: self.show_frame('Institutions'))
         button_institutions.pack()
         self.frames.update({'Menu': main_menu_frame})
 
-    def show_menu_frame(self):
-        print('Switching to main menu.')
-        self.show_frame('Menu')
-
     def add_persons_frame(self):
         persons_frame = Frame(self, bg="#CCCCCC")
-        # persons_frame.pack(fill=BOTH, expand=True)
         button = Button(persons_frame, text="You're in Persons")
         button.pack()
         self.frames.update({'Persons': persons_frame})
 
-    def show_persons_frame(self):
-        print('Switching to persons.')
-        self.show_frame('Persons')
-
     def add_institutions_frame(self):
         institutions_frame = Frame(self, bg="#EEAAAA")
-        # institutions_frame.pack(fill=BOTH, expand=True)
         button = Button(institutions_frame, text="You're in Institutions")
         button.pack()
         self.frames.update({'Institutions': institutions_frame})
 
-    def show_institutions_frame(self):
-        print('Switching to institutions.')
-        self.show_frame('Institutions')
-
     def show_frame(self, name):
+        print('Switching to frame "' + name + '".')
         if self.current_frame is not None:
             self.current_frame.pack_forget()
-        # for key, value in self.frames.items():
-        #    value.pack_forget()
         frame = self.frames[name]
         self.current_frame = frame
         frame.pack(fill=BOTH, expand=True)
-        # frame.tkraise()
 
 
 def show_window(controller):
