eb894345a377916b3c01f261a9090f30ff3eb343
Alexander <lowry.alex@live.com>
2019-12-27 20:28:10 -0600

    Changed frame-switching to be more efficient.

--- ProgramWindow.py
+++ ProgramWindow.py
@@ -8,6 +8,7 @@ class Window(Frame):
         self.master = master
         self.controller = controller
         self.frames = {}
+        self.current_frame = None
 
         self.pack(side="top", fill="both", expand=True)
         self.grid_rowconfigure(0, weight=1)
@@ -55,7 +56,11 @@ class Window(Frame):
 
     def add_menu_frame(self):
         main_menu_frame = Frame(self, bg="#DEDEDE")
-        main_menu_frame.pack(expand=True, fill='both')
+        # main_menu_frame.pack(expand=True, fill='both')
+        button_persons = Button(main_menu_frame, text="Persons", command=lambda: self.show_frame('Persons'))
+        button_persons.pack()
+        button_institutions = Button(main_menu_frame, text="Institutions", command=lambda: self.show_frame('Institutions'))
+        button_institutions.pack()
         self.frames.update({'Menu': main_menu_frame})
 
     def show_menu_frame(self):
@@ -64,8 +69,8 @@ class Window(Frame):
 
     def add_persons_frame(self):
         persons_frame = Frame(self, bg="#CCCCCC")
-        persons_frame.pack(fill=BOTH, expand=True)
-        button = Button(persons_frame, text="Persons")
+        # persons_frame.pack(fill=BOTH, expand=True)
+        button = Button(persons_frame, text="You're in Persons")
         button.pack()
         self.frames.update({'Persons': persons_frame})
 
@@ -75,8 +80,8 @@ class Window(Frame):
 
     def add_institutions_frame(self):
         institutions_frame = Frame(self, bg="#EEAAAA")
-        institutions_frame.pack(fill=BOTH, expand=True)
-        button = Button(institutions_frame, text="Institutions")
+        # institutions_frame.pack(fill=BOTH, expand=True)
+        button = Button(institutions_frame, text="You're in Institutions")
         button.pack()
         self.frames.update({'Institutions': institutions_frame})
 
@@ -85,9 +90,12 @@ class Window(Frame):
         self.show_frame('Institutions')
 
     def show_frame(self, name):
-        for key, value in self.frames.items():
-            value.pack_forget()
+        if self.current_frame is not None:
+            self.current_frame.pack_forget()
+        # for key, value in self.frames.items():
+        #    value.pack_forget()
         frame = self.frames[name]
+        self.current_frame = frame
         frame.pack(fill=BOTH, expand=True)
         # frame.tkraise()
 
