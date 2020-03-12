5e831a43ff79680f1bd1beb96a4ef101efefd5b3
Alexander Lowry <lowry.alex@live.com>
2020-03-12 10:51:06 -0500

    Minor cleaning.

--- start.py
+++ start.py
@@ -1,9 +1,5 @@
 from ProgramController import ProgramController
 import ProgramWindow
 
-# print(SkeletonDatabase.get_persons(2))
-# print(SkeletonDatabase.get_persons(3))
-
 controller = ProgramController()
 root, app = ProgramWindow.show_window(controller)
-# print("app: " + type(app))
