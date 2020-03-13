07bfc51dce44480be3b6e75c696bf89265381224
Alexander Lowry <lowry.alex@live.com>
2020-03-13 15:48:23 -0500

    Removed unused code for old Person frames.

--- ProgramWindow.py
+++ ProgramWindow.py
@@ -115,49 +115,8 @@ class Window(Frame):
         else:
             color = self.theme['List B']
         meta_frame = Frame(parent)
-        # frame = self.generate_person_frame_small(person, parent, index, meta_frame, None, color)
         person_frame = PersonFrame(person, self, self.theme, meta_frame, color=color)
         return meta_frame, person_frame
-
-    def generate_person_frame_small(self, person, parent, index, meta_frame, old_frame, color):
-        if old_frame is not None:
-            old_frame.pack_forget()
-        frame = Frame(meta_frame, bg=color)
-        frame.columnconfigure(1, weight=1)
-        frame.columnconfigure(2, weight=2)
-        frame.columnconfigure(3, weight=1)
-        name_text = person['fName'] + ' ' + person['lName']
-        name_label = Label(frame, text=name_text, padx=10, pady=10, bg=color, fg=self.theme['Text'])
-        name_label.grid(column=1, row=0)
-        expand_button = Button(frame, text="More", padx=10,
-                               command=lambda: self.generate_person_frame_large(
-                                   person, parent, index, meta_frame, frame, color))
-        expand_button.grid(column=3, row=0)
-        # new_button.grid(column=0, row=index)
-        frame.pack(fill=X, expand=True)
-        return frame
-
-    def generate_person_frame_large(self, person, parent, index, meta_frame, old_frame, color):
-        if old_frame is not None:
-            old_frame.pack_forget()
-        frame = Frame(meta_frame, bg=color)
-        frame.columnconfigure(1, weight=1)
-        frame.columnconfigure(2, weight=2)
-        frame.columnconfigure(3, weight=1)
-        frame.rowconfigure(0, weight=1)
-        frame.rowconfigure(1, weight=1)
-        frame.rowconfigure(2, weight=1)
-        frame.rowconfigure(3, weight=1)
-        frame.rowconfigure(5, weight=1)
-        name_text = person['fName'] + ' ' + person['lName']
-        name_label = Label(frame, text=name_text, padx=10, pady=60, bg=color, fg=self.theme['Text'])
-        name_label.grid(column=1, row=0)
-        expand_button = Button(frame, text="Less", padx=10,
-                               command=lambda: self.generate_person_frame_small(
-                                   person, parent, index, meta_frame, frame, color))
-        expand_button.grid(column=3, row=0)
-        # new_button.grid(column=0, row=index)
-        frame.pack(fill=X, expand=True)
 
     def add_institutions_frame(self):
         institutions_frame = Frame(self, bg=self.theme['Background'])
