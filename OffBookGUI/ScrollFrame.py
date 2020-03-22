1fa44e3816949e5ada2d8a0af4892cb35f10ffa3
Alexander Lowry <lowry.alex@live.com>
2020-03-22 02:56:12 -0500

    Got scrolling working, but with wrongly widthed list items.

--- OffBookGUI/ScrollFrame.py
+++ OffBookGUI/ScrollFrame.py
@@ -3,18 +3,24 @@ from tkinter import *
 
 class ScrollFrame(Frame):
     def __init__(self, view, holder_frame=None):
+        # Note to self: it's possible the items on the canvas are not attached to the canvas surface somehow, and
+        # therefore not scrolling.
+        # Or the canvas size
         super(ScrollFrame, self).__init__(holder_frame, background='#0000FF')
         self.view = view
         self.scrollbar = Scrollbar(self, orient='vertical')
-        self.canvas = Canvas(self, background='#00FF00')
-        self.canvas.config(yscrollcommand=self.scrollbar.set, scrollregion=self.canvas.bbox(ALL))
-        # self.child_frame = Frame(self.canvas, background='#FF0000', height=10000, width=1000)
+        self.canvas = Canvas(self, background='#00FF00', height=10000, width=10000)
+        self.child_frame = Frame(self.canvas, background='#FF0000')
+                                 #, height=10000, width=1000)
+        self.child_frame.bind('<Configure>', lambda e: self.canvas.configure(scrollregion=self.canvas.bbox(ALL)))
+        self.canvas.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.pack(side=RIGHT, fill=Y)
         self.scrollbar.config(command=self.canvas.yview)
-        # self.child_frame.pack(padx=10, pady=10)
-        self.canvas.pack(fill=BOTH, padx=10, pady=10)
-        self.items = []
+        self.canvas.pack(fill=BOTH)
+        self.child_frame.pack()
         self.canvas.bind_all('<MouseWheel>', self._on_mousewheel)
+        self.canvas.create_text(100, 100, text='hello world')
+        self.canvas.create_window((0, 0), window=self.child_frame, anchor='nw')
 
     def add(self, item):
         self.items.append(item)
@@ -23,16 +29,10 @@ class ScrollFrame(Frame):
     def get_canvas(self):
         return self.canvas
 
-    def reset_child(self):
-        self.items = []
-        self.child_frame.pack_forget()
-        self.child_frame.destroy()
-        # self.child_frame = Frame(self.canvas, background='#00FF00')
-
     def set_bbox(self):
         print('Bounding box',  self.canvas.bbox(ALL))
-        # self.canvas.config(scrollregion=self.canvas.bbox(ALL))
-        self.canvas.config(scrollregion=(0, 0, 10000, 10000))
+        self.canvas.config(scrollregion=self.canvas.bbox(ALL))
+        # self.canvas.config(scrollregion=(0, 0, 10000, 10000))
 
     def _on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), 'units')
